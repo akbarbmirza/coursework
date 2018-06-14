3c878b2222e3156c69280cebe83bb0e4172d9030
Akbar Mirza <akbarbmirza@gmail.com>
2018-06-14 17:21:27 -0400

    Update plot_trips solutions

--- week1/plot_trips.R
+++ week1/plot_trips.R
@@ -20,7 +20,9 @@ load('trips.RData')
 ########################################
 
 # plot the distribution of trip times across all rides
-ggplot(trips, aes(x = tripduration)) +
+
+filter(trips, tripduration < quantile(tripduration, 0.99)) %>% # throwing away top 1% of outliers
+  ggplot(aes(x = tripduration / 60)) +
   geom_histogram() +
   scale_x_log10(label = comma) +
   scale_y_continuous(label = comma) +
@@ -28,45 +30,57 @@ ggplot(trips, aes(x = tripduration)) +
   ylab('# of Riders')
 
 # plot the distribution of trip times by rider type
-ggplot(trips, aes(x = tripduration)) +
+filter(trips, tripduration < quantile(tripduration, 0.99)) %>% # throwing away top 1% of outliers
+  ggplot(aes(x = tripduration / 60)) +
   geom_histogram() +
   scale_x_log10(label = comma) +
   scale_y_continuous(label = comma) +
   xlab('Trip Times by Rider Type ') +
   ylab('# of Riders') +
-  facet_wrap(~ usertype, scale = "free")
+  facet_wrap(~ usertype, ncol = 1, scale = "free_y")
 
 # plot the total number of trips over each day
 trips_per_day <- trips %>% group_by(ymd) %>% summarize(count = n())
+
 ggplot(trips_per_day, aes(x = ymd, y = count)) +
-  geom_point() +
+  geom_line() +
   scale_y_continuous(label = comma) +
   xlab('Days of the Year') +
   ylab('Number of Trips')
 
-# plot the total number of trips (on the y axis) by age (on the x axis) and age (indicated with color)
+# plot the total number of trips (on the y axis) by age (on the x axis) and gender (indicated with color)
 trips_per_age <- trips %>%
   mutate(age = year(ymd) - birth_year) %>%
-  group_by(age) %>%
+  group_by(age, gender) %>%
   summarize(count = n())
 
 ggplot(trips_per_age, aes(x = age, y = count)) +
   scale_y_continuous(label = comma) +
-  geom_point(mapping = aes(color = age))
+  geom_point(mapping = aes(color = gender))
 
 # plot the ratio of male to female trips (on the y axis) by age (on the x axis)
 # hint: use the spread() function to reshape things to make it easier to compute this ratio
 
 trips_per_age_and_gender <- trips %>%
-  mutate(age = year(ymd) - birth_year) %>%
+  mutate(age = 2014 - birth_year) %>%
+  filter(age < 65) %>%
   group_by(age, gender) %>%
   summarize(count = n()) %>%
-  filter(gender != "Unknown" & age != "NA") %>%
   spread(gender, count)
+
+# alt solution
+# trips_per_age_and_gender <- trips %>%
+#   +     mutate(age = 2014 - birth_year) %>%
+#   +     filter(age < 65) %>%
+#   +     group_by(age, gender) %>%
+#   +     summarize(count = n()) %>%
+#   +     group_by(age) %>%
+#   +     filter(gender != "Unknown") %>%
+#   +     summarise(ratio = first(count) / last(count))
 
 ggplot(trips_per_age_and_gender, aes(x = age, y = Male / Female)) +
   geom_point() +
-  xlim(c(0, 90)) +
+  geom_smooth() +
   xlab("Rider Age") +
   ylab("Male to Female Ratio")
 
@@ -124,18 +138,18 @@ trips %>% mutate(hour = hour(starttime)) %>%
   summarize(num_trips = n()) %>%
   group_by(hour) %>%
   summarize(avg = mean(num_trips), sd = sd(num_trips)) %>%
-  gather("stat", "value", avg, sd) %>%
-  ggplot(aes(x = hour, y = value, color = stat)) +
-  geom_point()
+  ggplot(aes(x = hour, y = avg)) +
+  geom_line() + 
+  geom_ribbon(aes(ymin = avg - sd, ymax = avg + sd), alpha = 0.2)
 
 # repeat this, but now split the results by day of the week (Monday, Tuesday, ...) or weekday vs. weekend days
 # hint: use the wday() function from the lubridate package
-trips %>% mutate(day = wday(starttime), hour = hour(starttime)) %>%
+trips %>% mutate(day = wday(starttime, label = TRUE), hour = hour(starttime)) %>%
   group_by(ymd, day, hour) %>%
   summarize(num_trips = n()) %>%
   group_by(hour, day) %>%
   summarize(avg = mean(num_trips), sd = sd(num_trips)) %>%
-  gather("stat", "value",  avg, sd) %>%
-  ggplot(aes(x = hour, y = value, color = stat)) +
-  geom_point() +
+  ggplot(aes(x = hour, y = avg)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = avg - sd, ymax = avg + sd), alpha = 0.2) +
   facet_wrap(~ day)
